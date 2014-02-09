1faa6cf3e6b7ef944c4fddc81c437acafd1ab21e
manijain <manijain333@gmail.com>
2014-02-09 20:01:29 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JainManish::Application.config.secret_key_base = 'acc0521474d617198cae253a6073d79ce331ac470323ed0ee2f9b0a228fe99de0f58658cff842bb0ad4103ea79813acc4ff9e6f5f2e36cb44866b8fb2d8437bf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JainManish::Application.config.session_store :cookie_store, key: '_jain_manish_session'
