7650505f71299b4c88e77dfe65c796cba3af3c59
manijain <manijain333@gmail.com>
2014-02-11 00:02:50 +0530

    precompile assets added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,10 @@ JainManish::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  # config.assets.precompile << 'application.js'
+  # config.assets.precompile << 'application.css'
 
   # Generate digests for assets URLs.
   config.assets.digest = true
