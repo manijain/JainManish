fecef84e82a117fca8e3d9011ac89f22e548a54f
manijain <manijain333@gmail.com>
2014-02-23 23:39:21 +0530

    improve static pages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 	http_basic_authenticate_with name: "manish", password: "secret", only: :destroy
 
 	def create
-		@post = Post.find(params[:post_id])
+	@post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
     redirect_to post_path(@post)
 	end
