9cba0b7438ebfdbcb798a9c729a3c07068faeeb6
manish <manijain333@gmail.com>
2014-06-10 16:45:30 +0530

    improved with feedback form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ActiveRecord::Base.connection.execute("TRUNCATE users")
+status = User.create([{ :email => 'manijain333@gmail.com' },{ :password => 'pinkcity' }, { :password_confirmation => 'pinkcity' }])
+status.update_attributes(:is_admin => true)
